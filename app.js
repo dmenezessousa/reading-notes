217d020a9a05ae543afcbbaa2c86713249075b36
Diego Sousa <khesanh@diegos-mbp.myfiosgateway.com>
2022-08-26 10:17:27 -0400

    removed js from notes

--- app.js
+++
@@ -1,7 +0,0 @@
-let cookieForm = document.getElementById("cookie-form");
-
-cookieForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  let { cookie, cookie_type } = event.target;
-  console.log(cookie.value, cookie_type.value);
-});
